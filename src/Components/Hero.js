996b2de964eeb467a67d4a43102066c9bc0a985c
Behzod <bmoydinboyev@usf.edu>
2021-12-18 02:10:25 -0500

    add typewriter on hero-section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom';
-import HeroImg from '../images/girl-study.png'
+import Typewriter from 'typewriter-effect';
+import HeroImg from '../images/girl-study.png';
 
 function Hero() {
     return (
         <div className='hero'>
             <div className='hero-longcopy'>
-                <h1>We strive for equality...</h1> {/* change the last word using a type package */}
+                <h1>We strive for <br />
+                    <Typewriter
+                        options={{
+                            autoStart: true,
+                            strings: ['equality', 'quality', 'accessibility', 'education', 'better future'],
+                            loop: true
+                        }}
+                    />
+                </h1>
                 <p className='description'>We bridge the gap between students who need help outside of classes
                     and nonprofit organizations that offer free educational services.</p>
                 <Link to='/sign-up'>
