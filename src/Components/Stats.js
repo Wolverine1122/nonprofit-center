65b105c41096820b6c1121a503404d17dc77dfdd
Behzod <bmoydinboyev@usf.edu>
2021-12-19 00:42:27 -0500

    add animation for stats

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,7 +1,9 @@
+import CountUp from 'react-countup';
+
 function Stats(props) {
     return (
         <div>
-            <h3>{props.number}+</h3>
+            <h3>{<CountUp start={0} end={props.number} duration={2} suffix=" +" separator="," />}</h3>
             <p>{props.description}</p>
         </div>
     )
