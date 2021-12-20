b5d2f636663c7484e10d621c75953e8bfebb853e
Behzod <bmoydinboyev@usf.edu>
2021-12-19 20:06:40 -0500

    add sign-in page

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,6 +1,37 @@
+import { Link } from 'react-router-dom';
+
 function SignIn() {
     return (
-        <div className='sign-in-page'>SignIn</div>
+        <div className='sign-in-page'>
+            <div className="card">
+                <h2>Log in to your account</h2>
+                <form action="#" method="POST" className="login-form">
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="Email address"
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                        required
+                    />
+                    <button className="btn btn-full">Sign in</button>
+                </form>
+            </div>
+            <div className="cues">
+                <div>
+                    <Link to='/sign-up'>Create an account</Link>
+                </div>
+                <div>
+                    <a href="https://www.google.com/">Forgot password?</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
