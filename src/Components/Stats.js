026afbe56952d2131cb92950ef5fd7078b89d16a
Behzod <bmoydinboyev@usf.edu>
2022-01-02 17:47:22 -0500

    animate stats based on scroll

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,9 +1,20 @@
+import VisibilitySensor from 'react-visibility-sensor';
 import CountUp from 'react-countup';
 
 function Stats(props) {
     return (
         <div>
-            <h3>{<CountUp start={0} end={props.number} duration={2} suffix=" +" separator="," />}</h3>
+            <h3>
+                {
+                    <CountUp start={0} end={props.number} duration={2} suffix=" +" separator=",">
+                        {({ countUpRef, start }) => (
+                            <VisibilitySensor onChange={start} delayedCall>
+                                <span ref={countUpRef} />
+                            </VisibilitySensor>
+                        )}
+                    </CountUp>
+                }
+            </h3>
             <p>{props.description}</p>
         </div>
     )
