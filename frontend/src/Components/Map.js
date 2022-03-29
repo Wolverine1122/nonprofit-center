0d22a19cc38b2d48e7dcc42831540bc14e4530c9
aaronyii <aaronyii@mail.usf.edu>
2022-03-29 19:50:40 -0400

    added more non-profit organizations onto the map in Map.js

--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -1,47 +1,47 @@
-import GoogleMapReact from 'google-map-react';
-import TextSearchResults from '../Texts/TextSearchResults';
-import Marker from './Marker';
-import useGeolocation from '../Custom Hooks/useGetLocation';
+import GoogleMapReact from "google-map-react";
+import TextSearchResults from "../Texts/TextSearchResults";
+import Marker from "./Marker";
+import useGeolocation from "../Custom Hooks/useGetLocation";
 
 function createLocationEntry(location, index) {
-    return (
-        <Marker
-            key={index}
-            lat={location.latitude}
-            lng={location.longitude}
-            name={location.name}
-        />
-    )
+  return (
+    <Marker
+      key={index}
+      lat={location.latitude}
+      lng={location.longitude}
+      name={location.name}
+    />
+  );
 }
 
 function createMapOptions(maps) {
-    return {
-        mapTypeControl: true,
-        // gestureHandling: 'greedy' 
-        // uncomment this to allow touch-screen users to navigate the map
-        // with one finger. Default is auto, which determines if greedy is necessary on the device
-    }
+  return {
+    mapTypeControl: true,
+    // gestureHandling: 'greedy'
+    // uncomment this to allow touch-screen users to navigate the map
+    // with one finger. Default is auto, which determines if greedy is necessary on the device
+  };
 }
 
 function Map() {
-    const geoLoc = useGeolocation({
-        //use New York as default location
-        lat: 40.7128,
-        lng: -74.0060
-    })
+  const geoLoc = useGeolocation({
+    //use New York as default location
+    lat: 40.7128,
+    lng: -74.006,
+  });
 
-    return (
-        <div className='google-map'>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API }}
-                center={geoLoc.location}
-                defaultZoom={10}
-                options={createMapOptions}
-            >
-                {TextSearchResults.map(createLocationEntry)}
-            </GoogleMapReact>
-        </div>
-    )
+  return (
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API }}
+        center={geoLoc.location}
+        defaultZoom={10}
+        options={createMapOptions}
+      >
+        {TextSearchResults.map(createLocationEntry)}
+      </GoogleMapReact>
+    </div>
+  );
 }
 
 export default Map;
